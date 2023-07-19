691d614896e62522ac720a4121828b22430050c5
John Smith <pontorez@protonmail.ch>
2023-07-19 14:19:05 -0400

    Remove comment

--- registration.php
+++ registration.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Aheadgroups_Ordereditor
- * @category    Aheadgroups
- * @package     Aheadgroups_Ordereditor
- * @license     Commercial
-**/
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
